97244a792fffc88a3e633c5cf588b6b239bb390d
AntonioRamalho96 <antonio.ramalho84@gmail.com>
2025-05-24 17:59:06 +0200

    Added base for react router
     - run:
       - npm i react-router
     - modified app.tsx for example

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from 'react-dom/client';
+import { HashRouter, NavLink, Route, Routes } from 'react-router';
 
 function RenderApp()
 {
@@ -17,6 +18,16 @@ function RenderApp()
         <h1>Hello world</h1>
         <h2>A react example</h2>
         <button onClick={action_fn}>Click Me</button>
+        <HashRouter>
+            <NavLink to={"/"}>Home</NavLink>
+            <NavLink to={"/page_1"}>Page 1</NavLink>
+            <NavLink to={"/page_2"}>Page 2</NavLink>
+            <Routes>
+                <Route path='/' element={<h1>Home</h1>}/>
+                <Route path='/page_1' element={<h1>PageOne</h1>}/>
+                <Route path='/page_2' element={<h1>PageTwo</h1>}/>
+            </Routes>
+        </HashRouter>
         </>
     )
 }
